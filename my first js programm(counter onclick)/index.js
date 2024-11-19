28c845bd24a5459e917104ce72fa29a00a746aee
egan-ombaka <eganombaka97@gmail.com>
2024-11-19 23:36:09 +0300

    js

--- my first js programm(counter onclick)/index.js
+++ my first js programm(counter onclick)/index.js
@@ -14,3 +14,28 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
+
+
+
+
+
+
+
+
+let count = 0;
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+//console.log(countEl)
+function increment() {
+    count += 1;
+    countEl.textContent = count
+}
+
+function save() {
+    let saveDig = count + " - "
+    saveEl.textContent += saveDig
+    countEl.textContent = 0
+    count = 0
+}
--- second js programm two way odds calculator/script.js
+++ second js programm two way odds calculator/script.js
@@ -11,3 +11,26 @@ function arbitrageRate(){
 }
 
 arbitrageRate()
+
+
+
+
+
+
+
+let count = 0;
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+//console.log(countEl)
+function increment() {
+    count += 1;
+    countEl.textContent = count
+}
+
+function save() {
+    let saveDig = count + " - "
+    saveEl.textContent += saveDig
+    countEl.textContent = 0
+    count = 0
+}
